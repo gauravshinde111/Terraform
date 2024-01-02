13d71caa8f89d47bb76b624948715c084b525952
gauravshinde111 <144316197+gauravshinde111@users.noreply.github.com>
2024-01-02 11:09:48 +0530

    Update Instance1.tf

--- Instance1.tf
+++ Instance1.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_instance" "web" {
   ami                    = "ami-07ec4220c92589b40"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   key_name               = "gaurav"
   vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
   tags = {
